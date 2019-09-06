a9e5721e8bd7d622a80661b7a5aa5292d35f2855
justanotherguy-0 <50782673+justanotherguy-0@users.noreply.github.com>
2019-09-06 01:01:19 -0400

    Update EJERCICIO17.py

--- EJERCICIO17.py
+++ EJERCICIO17.py
@@ -1,13 +1,14 @@
 d=str((0,1,2,3,4,5,6,7,8,9))
 password=input("Password testing. Write your password: ")
 
-def number_detector(p):
+'''def number_detector(p):
     a=False
     for i in p:
         if d.count(i)>0:
             a=True    
-    return a        
-if not password.islower() and not password.isupper() and number_detector(p):
+    return a     '''   
+#number detector can be replaced by string method str.isdigit
+if not password.islower() and not password.isupper() and not password.isdigit():
     print('I can\'t hack you')
 else:
     print('Your bank account is mine( and so is your wife :v)')
